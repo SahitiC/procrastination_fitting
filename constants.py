f4ab81ae6e28a16fc03c17907984a1c918dfb273
SahitiC <csahiti07@gmail.com>
2025-10-22 17:04:36 +0200

    modify constants to match paper

--- constants.py
+++ constants.py
@@ -6,14 +6,14 @@ import numpy as np
 
 # define some standard params:
 # states of markov chain
-STATES_NO = 20+1  # one extra state for completing nothing
+STATES_NO = 22+1  # one extra state for completing nothing
 STATES = np.arange(STATES_NO)
 
 # actions = no. of units to complete in each state
 # allow as many units as possible based on state
 ACTIONS = [np.arange(STATES_NO-i) for i in range(STATES_NO)]
 
-HORIZON = 15  # no. of weeks for task
+HORIZON = 16  # no. of weeks for task
 DISCOUNT_FACTOR = 0.9  # discounting factor
 EFFICACY = 0.8  # self-efficacy (probability of progress for each unit)
 
